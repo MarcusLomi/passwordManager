0475d77f95ff04c06ff285f95b2ca7f54268e1aa
Brian Schillaci <bschillaci25@nwk-145-68.nwk.ruw.rutgers.edu>
2017-04-22 18:04:28 -0400

    added getters and setters for user

--- src/Model/User.java
+++ src/Model/User.java
@@ -14,5 +14,44 @@ public User(String username, String password, String email, ArrayList<Account> a
 	this.accounts = accounts;
 }
 
+public String getUsername() {
+	return username;
+}
+
+public void setUsername(String username) {
+	this.username = username;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getDate_of_birth() {
+	return date_of_birth;
+}
+
+public void setDate_of_birth(String date_of_birth) {
+	this.date_of_birth = date_of_birth;
+}
 
 }
