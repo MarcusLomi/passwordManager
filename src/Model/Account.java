9f43a6eb361a357ae27caaf9869ad88781f294c5
Brian Schillaci <bschillaci25@nwk-145-68.nwk.ruw.rutgers.edu>
2017-04-22 17:04:37 -0400

    adding user and account class changes

---
+++ src/Model/Account.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class Account {
+private String username, password, email;
+
+public Account(String username, String password, String email){
+	this.username = username;
+	this.password = password;
+	this.email = email;
+}
+	
+}
--- src/Model/User.java
+++ src/Model/User.java
@@ -1,5 +1,18 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class User {
+private String username, password, name, email, date_of_birth;
+
+private ArrayList<Account> accounts;
+
+public User(String username, String password, String email, ArrayList<Account> accounts){
+	this.username = username;
+	this.password = password;
+	this.email = email;
+	this.accounts = accounts;
+}
+
 
 }
