406d920c1936196ee42876938d002d7410997bcf
MarcusLomi <marc.a.lomi@gmail.com>
2017-04-22 19:35:49 -0400

    Password masking is up

--- src/Controller/CreateAccountController.java
+++ src/Controller/CreateAccountController.java
@@ -1,26 +1,27 @@
 package Controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class CreateAccountController {
 
-    @FXML
-    private TextField userTextField;
-
-    @FXML
-    private TextField passwordTextField;
-
-    @FXML
-    private TextField confirmPassTextField;
+	@FXML
+	private TextField userTextField;
+	
+	@FXML
+	private PasswordField passWordField;
+	
+	@FXML
+	private PasswordField confirmPasswordField;
 
     @FXML
     private TextField emailTextField;
-
     
     public void start(Stage mainstage){
     	mainstage.setTitle("Create Account");
+    	
     }
 }
 
