4d4f00ffc8f0b8ff4e1b2cfe0ab31770e99309d5
Brian Schillaci <bschillaci25@nwk-145-68.nwk.ruw.rutgers.edu>
2017-04-22 21:07:53 -0400

    added the window to add a new site account

--- src/Controller/AccountViewController.java
+++ src/Controller/AccountViewController.java
@@ -1,6 +1,13 @@
 package Controller;
+import java.io.IOException;
+
 import Model.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
@@ -18,6 +25,35 @@ public class AccountViewController {
     @FXML
     private TableColumn<User, String> commentColumn;
 
+    @FXML
+    private Button addSiteAccountPass;
+
+    @FXML
+    private Button deleteSiteAccount;
+
+    @FXML
+    private Button saveButton;
+    
+    @FXML
+    void openSiteAdder(ActionEvent event) {
+    	try {
+			Stage stage;
+			Parent root;
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/AddSiteView.fxml"));
+			root = loader.load();
+			stage = (Stage)this.addSiteAccountPass.getScene().getWindow();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			AddSiteAccountController ac = loader.getController();
+			ac.start(stage);
+			stage.show();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+    	
+    	
+    }
+    }
     
     public void start(Stage mainstage){
     	mainstage.setTitle("View Accounts");
---
+++ src/Controller/AddSiteAccountController.java
@@ -0,0 +1,43 @@
+package Controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class AddSiteAccountController {
+	  @FXML
+	    private TextField site;
+
+	    @FXML
+	    private TextField username;
+
+	    @FXML
+	    private TextField password;
+
+	    @FXML
+	    private TextField comments;
+	    
+	    @FXML
+	    private Button submitButton;
+
+	   // @FXML
+	    void addAccount(ActionEvent event) {
+	    	
+	    }
+	    
+	    @FXML
+	    void submit(ActionEvent event) {
+
+	    }
+	    
+	    public void start(Stage mainstage){
+	    	mainstage.setTitle("Add new account");
+	    }
+}
